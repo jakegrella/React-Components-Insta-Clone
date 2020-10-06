4b1b4f00c453850ad9dc3fe5ddb662d76ff90afd
Jake Grella <jgrella13@gmail.com>
2020-10-06 17:22:00 -0500

    completed app.js

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
       <SearchBar />
-      <Posts />
+      <Posts likePost={likePost} posts={posts}/>
     </div>
   );
 };
